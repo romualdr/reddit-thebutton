f7731df607700476f011bd2307caec549dede2e6
Romuald R <dafunix@gmail.com>
2015-04-09 01:57:50 +0200

    1.0.2 Updated codebase Global refactoring

--- background.js
+++ background.js
@@ -2,123 +2,200 @@
 	"use strict";
 	
     // Setting first values - in case reddit doesn't answer ...
-	window.url = "wss://wss.redditmedia.com/thebutton?h=b915df7048013948fef513d53f58b38b3eecf7fc&e=1428579198";
-	window.socket = null;
-	window.state = {
-		connected: false,
-		left: NaN,
-		participants: 0
-	};
 	window.actions = {};
+
+
+    // Update the label
+    function updateIcon() {
+        var time = StateMachine.get('left');
+        if (!StateMachine.get('started'))
+            return chrome.browserAction.setBadgeText({text: ""});
+        if (!time)
+            return;
+        time = time - 1;
+        chrome.browserAction.setBadgeText({text: " " + time + " "});
+
+        var backgroundColor;
+
+        // 51+
+        if (time > 50) {
+            backgroundColor = "#820080";
+        } 
+        // 41 - 50
+        else if (time > 40) {
+            backgroundColor = "#0083C7";
+
+        }
+        // 31 - 40
+        else if (time > 30) {
+            backgroundColor = "#02be01";
+        }
+        // 21 - 30
+        else if (time > 20) {
+            backgroundColor = "#E5D900";
+        }
+        // 11 - 20
+        else if (time > 10) {
+            backgroundColor = "#e59500";
+        }
+        else if (time > 0) {
+            backgroundColor = "#e50000";
+        }
+        chrome.browserAction.setBadgeBackgroundColor({color: backgroundColor});
+    }
 
     /*
         @desc: State management
-        TODO: Maybe this function should send an update to popup ... would be nicer
     */
-    function updateState(key, value) {
-    	window.state[key] = value;
-    };
+    var StateMachine = (function () {
+        var _state = {
+            connected: false,
+            time_left: NaN,
+            participants: 0,
+            started: true
+        };
+        var _methods = {};
 
-    function getState(key) {
-    	return window.state[key];
-    };
+        _methods.get = function (key) {
+            if (key)
+                return _state[key];
+            return _state;
+        };
 
-    // Update the label
-    function updateTime() {
-    	var time = getState('left');
-    	if (!time)
-    		return;
-    	chrome.browserAction.setBadgeText({text: " " + (time - 1) + " "});
+        _methods.del = function (key) {
+            delete _state[key];
+        };
 
-    	var backgroundColor;
+        _methods.set = function (key, value, dontUpdate) {
+            _state[key] = value;
+            if (!dontUpdate)
+                this.update();
+            return this.get(key);
+        };
 
-    	// 51+
-    	if (time > 50) {
-    		backgroundColor = "#820080";
-    	} 
-    	// 41 - 50
-    	else if (time > 40) {
-    		backgroundColor = "#0083C7";
+        _methods.bulk = function (state) {
+            for (var key in state) {
+                _methods.set(key, state[key], true);
+            }
+            this.update();
+            return this.get();
+        };
 
-    	}
-    	// 31 - 40
-    	else if (time > 30) {
-    		backgroundColor = "#02be01";
-    	}
-    	// 21 - 30
-    	else if (time > 20) {
-    		backgroundColor = "#E5D900";
-    	}
-    	// 11 - 20
-    	else if (time > 10) {
-    		backgroundColor = "#e59500";
-    	}
-    	else if (time > 0) {
-    		backgroundColor = "#e50000";
-    	}
-    	chrome.browserAction.setBadgeBackgroundColor({color: backgroundColor});
-    }
+        _methods.update = function () {
+            updateIcon();
+            chrome.runtime.sendMessage({ action: "refresh", state: this.get() });
+        };
 
-    function start() {
-    	if (window.socket)
-    		window.socket.close();
-    	var socket = new WebSocket(window.url);
-    	socket.onopen = function(){
-	        updateState('connected', true);
-	      	chrome.runtime.sendMessage({action: "started"});
-	    };
+        return _methods;
+    })();
 
-	    socket.onmessage = function(message){
-	    	var json = JSON.parse(message.data);
-	    	updateState('left', json.payload.seconds_left);
-	    	updateState('participants', json.payload.participants_text);
-	    	window.actions['refresh']();
-	    };
-	    socket.onclose = function() {
-	    	updateState('connected', false);
-	    };
-	    window.socket = socket;
-    }
+    /*
+        @desc: Reddit connector
+    */
+    var RedditConnector = (function () {
+        var URL = null;
+        var SOCKET = null;
+        var _methods = {};
 
+        /*
+            Private methods
+        */
+
+        function updateURL(callback) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://www.reddit.com/r/thebutton/", true);
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState == 4) {
+
+                // Get the new thebutton websocket url and discard everything else
+                if (xhr.responseText.indexOf('wss://wss.redditmedia.com/thebutton') !== -1) {
+                    var url = xhr.responseText.substr(xhr.responseText.indexOf('wss://wss.redditmedia.com/thebutton'), 100);
+                    url = url.substr(0, url.indexOf('",'));
+                    URL = url;
+                    StateMachine.del('error');
+                    return callback(null, url);
+                }
+                StateMachine.set('error', 'errUnableToGetUrl');
+                return callback(true);
+              }
+            }
+            xhr.send();
+        }
+
+        /*
+            Public methods
+        */
+
+        _methods.connect = function () {
+            updateURL(function (err) {
+                if (err)
+                    return;
+                var socket = new WebSocket(URL);
+                socket.onopen = function(){
+                    StateMachine.set('connected', true);
+                };
+
+                socket.onmessage = function(message){
+                    var json = JSON.parse(message.data);
+                    StateMachine.bulk({
+                        connected: true,
+                        left: json.payload.seconds_left,
+                        participants: json.payload.participants_text
+                    });
+                };
+                socket.onclose = function() {
+                    StateMachine.set('connected', false);
+                };
+                SOCKET = socket;
+            });
+        };
+
+        _methods.disconnect = function () {
+            if (SOCKET)
+                SOCKET.close();
+            SOCKET = null;
+            StateMachine.set('connected', false);
+        };
+
+        _methods.reload = function () {
+            this.disconnect();
+            this.connect();
+        };
+        return _methods;
+    })();
+    
     /*
         @desc:  Restart function
                 Get a new socket URL
     */
-    window.actions['restart'] = function () {
-    	var xhr = new XMLHttpRequest();
-		xhr.open("GET", "http://www.reddit.com/r/thebutton/", true);
-		xhr.onreadystatechange = function() {
-		  if (xhr.readyState == 4) {
-
-            // Get the new thebutton websocket url and discard everything else
-		  	if (xhr.responseText.indexOf('wss://wss.redditmedia.com/thebutton') !== -1) {
-			    var url = xhr.responseText.substr(xhr.responseText.indexOf('wss://wss.redditmedia.com/thebutton'), 100);
-			    url = url.substr(0, url.indexOf('",'));
-			    window.url = url;
-			}
-            // Start the socket connection
-			start();
-		  }
-		}
-		xhr.send();
-		return;
+    window.actions['doRestart'] = function () {
+        StateMachine.set('started', true);
+        return RedditConnector.reload();
     };
 
-    window.actions['refresh'] = function () {
-    	updateTime();
-    	chrome.runtime.sendMessage(window.state);
+    window.actions['doStop'] = function () {
+        StateMachine.set('started', false);
+        return RedditConnector.disconnect();
+    };
+
+    window.actions['getState'] = function (message, callback) {
+        callback(null, StateMachine.get());
+    };
+
+    window.actions['doRefresh'] = function () {
+        return StateMachine.update();
     };
 
     /*
         @desc: Listen for popup action
         TODO: { popup: "action" } to be able to get only popup / action and action namespacing
     */
-    chrome.runtime.onMessage.addListener(function (message, sender, reponse) {
+    chrome.runtime.onMessage.addListener(function (message, sender, cb) {
 		if (message.action && window.actions[message.action]) {
-			window.actions[message.action]();
+			window.actions[message.action](message, cb);
 		}
 	});
 
-    window.actions['restart']();
+    RedditConnector.connect();
 
 })();
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 window.actions = {};
+window.triggers = {};
 // Analytics
 var _gaq = window._gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-61700114-1']);
@@ -10,44 +11,41 @@ _gaq.push(['_trackPageview']);
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 // !Analytics
-var state = {};
+
+
+var STATE = { connected: false };
+var INTERNALS = { page: "ext" };
+var ELS = {
+	"#time-count": null,
+	'#participants-count': null,
+	'#startscreen': null,
+	'body': null
+};
 
 
 /*
 	Update GUI with new data
 */
-function updateGUI(_state) {
-	if (_state.left !== state.left)
-		document.querySelector('#time-count').innerHTML = _state.left - 1;
-	if (_state.participants !== state.participants)
-		document.querySelector('#participants-count').innerHTML = _state.participants;
-	if (_state.connected !== state.connected) {
-		var el = document.querySelector('#restartscreen');
-		if (!_state.connected)
-			el.style.display = "block";
-		else
-			el.style.display = "none";
-	}
-	state = _state;
+function updateGUI() {
+	ELS['#time-count'].innerHTML = (STATE.left ? STATE.left - 1 : "??");
+	ELS['#participants-count'].innerHTML = (STATE.participants ? STATE.participants : "??");
+	ELS['#startscreen'].style.display = (STATE.connected && STATE.started ? "none" : "block");
+	ELS['#startscreen'].innerHTML = (STATE.started ? (STATE.connected ? "" : "Working ...") : "Stopped");
+	ELS["body"].className = "page-" + INTERNALS.page + " " + (STATE.started ? "started" : "stopped");
 }
-
-/*
-	@desc: Listen to message from background
-*/
-chrome.runtime.onMessage.addListener(function (message, sender, reponse) {
-	if (message.action) {
-		if (window.actions[message.action])
-			return window.actions[message.action](message);
-		return;
-	}
-	updateGUI(message);
-});
 
 /*
 	@desc: Bind buttons - Invoke tab creation when clicked
 	2 links : My twitter + sub-reddit /thebutton
 */
 document.addEventListener("DOMContentLoaded", function(event) { 
+	initializeLinks();
+	initializeELS();
+
+	chrome.runtime.sendMessage({ action: "doRefresh" });
+});
+
+function initializeLinks() {
 	var links = document.querySelectorAll('._link');
 	for (var i = 0; i < links.length; i++) {
 		links[i].onclick = (function (el) {
@@ -59,22 +57,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		})(links[i]);
 	};
 
-	var restart = document.querySelector('.restart');
-	restart.onclick = function () {
-		chrome.runtime.sendMessage({ action: "restart" });
+	var _triggers = document.querySelectorAll('._trigger');
+	for (var i = 0; i < _triggers.length; i++) {
+		_triggers[i].onclick = (function (el) {
+			var trigger = el.attributes['data-trigger'].value;
+			if (!window.triggers[trigger])
+				return undefined;
+			return function (e) {
+				var val = el.attributes['data-trigger-value'];
+				window._gaq.push(['_trackEvent', trigger, 'clicked']);
+				window.triggers[trigger]((val ? val.value : undefined));
+			}
+		})(_triggers[i]);
+	};
+}
+
+function initializeELS() {
+	for (var key in ELS) {
+		ELS[key] = document.querySelector(key);
 	}
+}
+
+/*
+	@desc: I/O with background
+		   User -> trigger
+*/
+window.triggers['restart'] = function () {
+	chrome.runtime.sendMessage({ action: "doRestart" });
+};
+
+window.triggers['changePage'] = function (page) {
+	INTERNALS.page = page;
+	updateGUI();
+};
+
+window.triggers['stop'] = function () {
+	chrome.runtime.sendMessage({ action: "doStop" });
+};
+
+window.triggers['start'] = function () {
+	chrome.runtime.sendMessage({ action: "doRestart" });
+}
+
+
+/*
+	@desc: I/O with background
+	       Background -> actions
+*/
+window.actions['refresh'] = function (message) {
+	STATE = message.state;
+	updateGUI();
+};
+
+chrome.runtime.onMessage.addListener(function (message, sender, reponse) {
+	if (message.action && window.actions[message.action])
+		return window.actions[message.action](message);
 });
-
-window.actions['restart'] = function () {
-	window._gaq.push(['_trackEvent', 'restart', 'clicked']);
-	var el = document.querySelector('#restartscreen');
-	el.style.display = "block";
-};
-
-window.actions['started'] = function () {
-	var el = document.querySelector('#restartscreen');
-	el.style.display = "none";
-};
-
-// Get state
-chrome.runtime.sendMessage({ action: "refresh" });
